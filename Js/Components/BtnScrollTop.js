446dea03fcaa1c170a9e6e462c37c73189da4610
Jano Martino <martinojano0@gmail.com>
2022-09-14 16:03:34 -0300

    Update BtnScrollTop.js

--- Js/Components/BtnScrollTop.js
+++ Js/Components/BtnScrollTop.js
@@ -2,7 +2,7 @@ export function BtnScrollTop() {
   const $body = document.querySelector("body"),
     $scrollTop = document.createElement("a");
   $scrollTop.classList = "scrollTop hidden";
-  $scrollTop.innerHTML = "&#xF148";
+  $scrollTop.textContent = "⬆";
 
   window.addEventListener("scroll", () => {
     let limite = $body.getBoundingClientRect();
