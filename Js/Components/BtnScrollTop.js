ab52df8a91a43f8f587871701ef8f1c028664159
Jano Martino <martinojano0@gmail.com>
2022-09-15 11:50:33 -0300

    Update BtnScrollTop.js

--- Js/Components/BtnScrollTop.js
+++ Js/Components/BtnScrollTop.js
@@ -14,7 +14,7 @@ export function BtnScrollTop() {
   });
 
   document.addEventListener("click", (e) => {
-    if (e.target.matches(".scrollTop")) {
+       if (e.target.matches(".scrollTop") || e.target.matches(".arrow-5")) {
       window.scrollTo(0, 0);
     }
   });
