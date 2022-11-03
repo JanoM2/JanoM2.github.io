a648a5eb62e792bdd2e04f1f26bbc09893650989
Jano Martino <martinojano0@gmail.com>
2022-11-03 13:18:06 -0300

    update in btnDownload

--- Js/Components/BtnDownload.js
+++ Js/Components/BtnDownload.js
@@ -1,8 +1,8 @@
 export default function BtnDownload() {
   const $btnDownload = document.createElement("a");
   $btnDownload.classList.add("home--btnDownload");
-  $btnDownload.href = "/assets/Jano Martino.pdf";
-  $btnDownload.download = "Jano Martino";
+  $btnDownload.href = "/assets/Jano Martino - FrontEnd Dev en.pdf";
+  $btnDownload.download = "Jano Martino - FrontEnd Dev";
   $btnDownload.textContent = "Descargar Cv";
 
   return $btnDownload;
